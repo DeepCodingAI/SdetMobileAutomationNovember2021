b4446d43dc5112c5c2b090250b9444726c34d4d5
mrahman <rahmanww@gmail.com>
2022-02-06 21:23:07 -0500

    appium,nyp

---
+++ NYP/src/test/java/nyptest/TestArticles.java
@@ -0,0 +1,15 @@
+package nyptest;
+
+import base.MobileAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pages.MainPage;
+
+public class TestArticles extends MobileAPI {
+
+    @Test
+    public void testArticles(){
+        MainPage mainPage = PageFactory.initElements(appiumDriver, MainPage.class);
+        mainPage.goToArticles();
+    }
+}
